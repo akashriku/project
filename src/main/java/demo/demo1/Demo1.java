6f304263ec6d4aa29e70aaf884bfd4c2738b9905
Akash.S <Akash.S@ICPL11957.infostretch.com>
2018-02-13 16:42:14 +0530

    commiting the code

---
+++ src/main/java/demo/demo1/Demo1.java
@@ -0,0 +1,23 @@
+package demo.demo1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	@Test
+	public void test1() {
+		
+         String url="https://www.makemytrip.com/flights/";
+		
+		 //to set property for the respective browse
+         System.setProperty("webdriver.chrome.driver","C:\\Users\\akash.s\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	     System.out.println("i am testng test");
+         WebDriver driver=new ChromeDriver();
+		 driver.get(url);
+		 
+
+	}
+
+}
---
+++ src/main/java/demo/demo1/ListnerDemo.java
@@ -0,0 +1,56 @@
+package demo.demo1;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListnerDemo implements ISuiteListener, ITestListener {
+
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ISuite arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ISuite arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
